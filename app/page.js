9309f02093ca68685ad460e15f62a2620d1dd231
oufii <awfi2006sofi@gmail.com>
2026-09-08 01:55:39 +0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 'use client';
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react'; 
 import { supabase } from '../lib/supabase';
 
 export default function Home() {
